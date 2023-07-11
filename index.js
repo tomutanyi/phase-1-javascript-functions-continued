98300623f06985d8572af4efe226c790768c31c3
tomutanyi <tomutanyi@gmail.com>
2023-07-11 23:56:29 +0300

    0 tests remaining

--- index.js
+++ index.js
@@ -9,3 +9,10 @@ function mondayWork(lawyer = 'go to the office', target = "This Monday"){
     return (`${target}, I will ${lawyer}.`)
 
 }
+
+function wrapAdjective (special = '||'){
+    return function (adjective = "special"){
+        return `You are ${special}${adjective}${special}!`;
+
+    };
+}
