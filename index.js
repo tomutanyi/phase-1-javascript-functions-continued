e4bbf9ca0d7e74b703c6c99cddced293678709dd
tomutanyi <tomutanyi@gmail.com>
2023-07-11 23:47:39 +0300

    2 tests remaining

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function saturdayFun(lawyer = 'roller-skate', target = "This Saturday"){
+    return (`${target}, I want to ${lawyer}!`)
+
+}
+
+
+function mondayWork(lawyer = 'go to the office', target = "This Monday"){
+    return (`${target}, I will ${lawyer}.`)
+
+}
